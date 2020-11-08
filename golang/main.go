655be3b1523278998aa5323f76108ef8f03925fe
Yusaku Matsuki <ymatzki@gmail.com>
2020-11-08 18:21:03 +0900

    feat: use SetNewRecordOp instead of private function

--- golang/main.go
+++ golang/main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/google/uuid"
 	"github.com/kjk/notionapi"
 )
 
@@ -38,13 +37,13 @@ func main() {
 	root := page.Root()
 
 	// Create new page
-	newBlock, newBlockOp := setNewRecordOp(userResp.User.ID, root, notionapi.BlockPage)
+	newBlock, newBlockOp := client.SetNewRecordOp(userResp.User.ID, root, notionapi.BlockPage)
 	ops := []*notionapi.Operation{newBlockOp}
 	ops = append(ops, newBlock.SetTitleOp(fmt.Sprintf("new page %d", now())))
 	ops = append(ops, root.ListAfterContentOp(newBlock.ID, ""))
 
 	// Add text in new page
-	childBlock, childBlockOp := setNewRecordOp(userResp.User.ID, newBlock, notionapi.BlockText)
+	childBlock, childBlockOp := client.SetNewRecordOp(userResp.User.ID, newBlock, notionapi.BlockText)
 	ops = append(ops, childBlockOp)
 	ops = append(ops, childBlock.SetTitleOp(fmt.Sprintf("new text %d", now())))
 	ops = append(ops, newBlock.ListAfterContentOp(childBlock.ID, ""))
@@ -53,41 +52,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func setNewRecordOp(userID string, parent *notionapi.Block, recordType string) (newBlock *notionapi.Block, operation *notionapi.Operation) {
-	newID := uuid.New().String()
-	now := now()
-
-	newBlock = &notionapi.Block{
-		ID:          newID,
-		Version:     1,
-		Alive:       true,
-		Type:        recordType,
-		CreatedBy:   userID,
-		CreatedTime: now,
-		ParentID:    parent.ID,
-		ParentTable: notionapi.TableBlock,
-	}
-
-	operation = &notionapi.Operation{
-		ID:      newBlock.ID,
-		Table:   notionapi.TableBlock,
-		Path:    []string{},
-		Command: notionapi.CommandSet,
-		Args: map[string]interface{}{
-			"id":           newBlock.ID,
-			"version":      newBlock.Version,
-			"alive":        newBlock.Alive,
-			"type":         newBlock.Type,
-			"created_by":   newBlock.CreatedBy,
-			"created_time": newBlock.CreatedTime,
-			"parent_id":    newBlock.ParentID,
-			"parent_table": newBlock.ParentTable,
-		},
-	}
-
-	return
 }
 
 func now() int64 {
